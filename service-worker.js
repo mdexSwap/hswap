5e8c45dd2a9b528927925acdfff3781b87aacf52
mdexswap <mdexswap@protonmail.com>
2021-11-02 20:58:03 +0800

    update
    add default token on swap

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://cdn.jsdelivr.net/gh/mdexSwap/hswap@main/precache-manifest.fa4a49024cb801274e1ab955edb2c7c2.js"
+  "https://cdn.jsdelivr.net/gh/mdexSwap/hswap@main/precache-manifest.070164e9c01d1a9581cd641599544282.js"
 );
 
 self.addEventListener('message', (event) => {
